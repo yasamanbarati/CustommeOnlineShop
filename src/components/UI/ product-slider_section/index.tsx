177593080c025bca4c4b7879829570f706eb1eb8
yasamanbarati <yasamanbarati81@gmail.com>
2024-07-25 17:58:13 +0330

    feat:development product slider section

---
+++ src/components/UI/ product-slider_section/index.tsx
@@ -0,0 +1,91 @@
+"use client";
+import { ReactNode, useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { SwiperSlide } from "swiper/react";
+import { Box, styled } from "@mui/material";
+
+import { allProductsType } from "@/_slice/type";
+import { ProductCard } from "@/components/cards";
+import TitlesSection from "@/components/section_titles";
+import SwiperSection from "@/components/swiper";
+
+interface Props {
+  title: string;
+  titleIcon: ReactNode;
+  queryProductFn: Promise<any>;
+  queryProductKey: string;
+  filterKey?: string;
+}
+
+const MainSection = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  height: "300px",
+  "& .swiper-section": {
+    width: "100%",
+    "& .swiper-wrapper": {
+      gap: "8px",
+    },
+    "& .swiper-slide": {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: "12px",
+      maxWidth: "250px",
+      height: "300px",
+    },
+  },
+}));
+
+const ProductSliderSection = ({
+  title,
+  titleIcon,
+  queryProductFn,
+  queryProductKey,
+  filterKey,
+}: Props) => {
+  const [product, setProduct] = useState<allProductsType[]>([]);
+
+  const { data, isLoading } = useQuery({
+    queryKey: [queryProductKey],
+    queryFn: () => queryProductFn,
+  });
+
+  useEffect(() => {
+    if (!isLoading && data) {
+      setProduct(data);
+      if (filterKey) {
+        const newData = data.filter((item: any) => item.category === filterKey);
+        setProduct(newData);
+      }
+    }
+  }, [data, isLoading]);
+
+  return (
+    <Box>
+      <TitlesSection title={title} titleIcon={titleIcon}>
+        <MainSection>
+          <SwiperSection>
+            {product.map((item) => {
+              return (
+                <SwiperSlide>
+                  <ProductCard
+                    id={item.id}
+                    image={item.image}
+                    price={item.price}
+                    title={item.title}
+                    count={item.count}
+                    discount={item.discount}
+                  />
+                </SwiperSlide>
+              );
+            })}
+          </SwiperSection>
+        </MainSection>
+      </TitlesSection>
+    </Box>
+  );
+};
+
+export default ProductSliderSection;
